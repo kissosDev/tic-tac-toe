a3eb0aa0820c8e02d432be3df41b86eec1f8a52a
kissosDev <kissosDev>
2022-05-25 21:30:29 +0300

    update UI

--- src/index.js
+++ src/index.js
@@ -1,49 +1,94 @@
-function initiateBoard() {
-  let board = [];
+// GAME STATUS
+const DRAW = "draw";
+const CURRENT_PLAYER_WON = "currentPlayerWon";
+const ONGOING = "ongoing";
+
+let board = getNewBoard();
+let currentPlayer = true;
+const cellElements = document.querySelectorAll(".box");
+// let index = 0;
+cellElements.forEach((cell, index) => {
+  cell.setAttribute("position", index);
+  // index++;
+  cell.addEventListener("click", handleClick);
+});
+const restartButton = document.querySelector("#restart_Game");
+restartButton.addEventListener("click", restartGame);
+
+function handleClick(event) {
+  // placeMark(cell, currentMarker);
+  const clickedPosition = event.target.getAttribute("position");
+  handleCurrentTurn(clickedPosition);
+  updateUI();
+}
+
+function updateUI() {
+  placeMarker();
+  highlightPlayerTurn();
+}
+
+function placeMarker() {
+  // for (let i = 0; i < 9; i++) {
+  //   if (board[i] === -1) continue;
+
+  //   cellElements[i].innerHTML = board[i];
+  // }
+
+  board.forEach((element, index) => {
+    cellElements[index].innerHTML = element === -1 ? "" : element;
+  });
+}
+
+function highlightPlayerTurn() {
+  const playerO = document.querySelector(
+    ".dashboard-container .dashboard-layout .playerO div"
+  );
+  const playerX = document.querySelector(
+    ".dashboard-container .dashboard-layout .playerX div"
+  );
+  if (!currentPlayer) {
+    playerO.setAttribute("style", "font-weight: 800");
+    playerX.setAttribute("style", "font-weight: 400");
+  } else {
+    playerX.setAttribute("style", "font-weight: 800");
+    playerO.setAttribute("style", "font-weight: 400");
+  }
+}
+
+function handleCurrentTurn(selectedPosition) {
+  if (!isAvailable(selectedPosition)) return; //update
+  board[selectedPosition] = getCurrentPlayerMarker();
+  console.log(board);
+  const currentStatus = getCurrentGameStatus();
+  if (currentStatus === ONGOING) {
+    currentPlayer = !currentPlayer;
+    return;
+  }
+  if (currentStatus === CURRENT_PLAYER_WON) {
+    declareWinner();
+    return;
+  }
+  if (currentStatus === DRAW) {
+    console.log("Its a draw");
+    return;
+  }
+}
+
+function getNewBoard() {
+  const board = [];
   for (let index = 0; index < 9; index++) {
     board.push(-1);
   }
   return board;
 }
 
-function getCurrentPlayerMarker(currentPlayer) {
-  if (currentPlayer) return "X"; // if currentPlayer is 0 this will evaluate to false
-  return "O";
+function getCurrentPlayerMarker() {
+  return currentPlayer ? "X" : "O";
 }
 
-// const turnToMarkerDict = {
-//   1: 'X',
-//   0: 'O'
-// };
+const isAvailable = (position) => board[position] === -1;
 
-function updateBoardAccordingToPlayerChoice(
-  board,
-  currentPlayer,
-  selectedPosition
-) {
-  const marker = getCurrentPlayerMarker(currentPlayer);
-  // const marker = turnToMarkerDict[currentPlayer];
-  board[selectedPosition] = marker;
-}
-
-// function switchPlayerTurn(currentPlayer, gameData) {
-
-//     if (getCurrentPlayerMarker(currentPlayer) === "X") {
-//         gameData.playerTurn = false;
-//     }
-//     gameData.playerTurn = true;
-
-// gameData.playerTurn = !gameData.playerTurn;
-// }
-function switchPlayerTurn(gameData) {
-  gameData.playerTurn = !gameData.playerTurn;
-  return gameData.playerTurn;
-}
-function isBoardFull(board) {
-  return !board.includes(-1);
-}
-
-function isGameOver(board) {
+function getCurrentGameStatus() {
   function shouldWin(i, j, k) {
     const x = board[i];
     const y = board[j];
@@ -52,78 +97,25 @@ function isGameOver(board) {
     return x === y && y === z;
   }
 
-  for (let i = 0; i < 6; i += 3) {
-    if (shouldWin(i, i + 1, i + 2)) return true;
+  for (let i = 0; i < 7; i += 3) {
+    if (shouldWin(i, i + 1, i + 2)) return CURRENT_PLAYER_WON;
   }
 
   for (let i = 0; i < 3; i++) {
-    if (shouldWin(i, i + 3, i + 6)) return true;
+    if (shouldWin(i, i + 3, i + 6)) return CURRENT_PLAYER_WON;
   }
-  if (shouldWin(0, 4, 8)) return true;
-  if (shouldWin(2, 4, 6)) return true;
+  if (shouldWin(0, 4, 8)) return CURRENT_PLAYER_WON;
+  if (shouldWin(2, 4, 6)) return CURRENT_PLAYER_WON;
   // both players did not win
-  return !board.includes(-1);
+  return board.includes(-1) ? ONGOING : DRAW;
 }
 
-function getUserLocation() {
-  let userInput = prompt(
-    "Pick location from 0-8 or q to exit game"
-  ).toLowerCase();
-  if (userInput === "q") return userInput;
-
-  const location = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-  if (location.includes(userInput)) return parseInt(userInput);
-
-  console.log("Invalid value has been given please choose other one");
-  return getUserLocation();
-}
-
-// function getPositionFromUser() {
-//   let userInput = 42;
-//   const location = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "q"];
-//   while (!location.includes(userInput)) {
-//     userInput = prompt("salooo").toLowerCase();
-//   }
-//   if (userInput === "q") return userInput;
-//   return parseInt(userInput);
-// }
-
-function declareWinner(currentPlayer) {
+function declareWinner() {
   let winner = currentPlayer ? "1" : "2";
   console.log(`player ${winner} is the winner!`);
 }
 
-function main() {
-  const board = initiateBoard();
-  const isAvailable = (position) => board[position] === -1;
-  const gameData = {
-    playerTurn: true,
-  };
-  let currentPlayer = gameData.playerTurn; // DOM
-  let currentMarker = getCurrentPlayerMarker(currentPlayer);
-  let selectedPosition = 10; //DOM
-
-  while (!isGameOver(board)) {
-    while (true) {
-      selectedPosition = getUserLocation();
-      if (selectedPosition === "q") {
-        console.log("Game is over bitch");
-        return;
-      }
-      if (isAvailable(selectedPosition)) break;
-      console.log("This postion is already taken");
-    }
-
-    updateBoardAccordingToPlayerChoice(board, currentPlayer, selectedPosition);
-    currentPlayer = switchPlayerTurn(gameData);
-    currentMarker = getCurrentPlayerMarker(currentPlayer);
-    console.log(board);
-  }
-  currentPlayer = switchPlayerTurn(gameData);
-  if (isBoardFull(board)) {
-    console.log("it's a draw!");
-    return;
-  }
-  declareWinner(board, currentPlayer);
-  console.log("game over");
+function restartGame() {
+  board = getNewBoard();
+  placeMarker();
 }
